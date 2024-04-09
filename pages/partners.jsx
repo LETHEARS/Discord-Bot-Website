b9124af16eda1d3f50ea9e6e93bc98a75e467eab
LETHEARS <166290815+LETHEARS@users.noreply.github.com>
2024-04-09 23:05:10 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -23,7 +23,7 @@ export default function Partners() {
                                 <h1 className="leading-none text-3xl font-bold text-white">ARİLENS</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                      ARİLENS. En İyi En Yakın Arkadaşımdır.
+                                      ARİLENS. En Ama En Ama En Yakın Arkadaşımdır.
                                     </p>
                                 </div>     
                             </div>
